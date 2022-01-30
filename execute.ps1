8137d94ef786c17b0439145d65629a8be6a8ac68
gamari <gamari4552@gmail.com>
2022-01-30 22:08:52 +0900

    型の付与。

--- execute.ps1
+++ execute.ps1
@@ -1,19 +1,20 @@
 ﻿function Main {   
     # TODO 環境変数から取得するようにする
-    $targetDir = "C:\Users\tamay\Downloads";
-    $outputDir = "C:\Users\tamay\Desktop\home\download\exe"
+    [string] $targetDir = "C:\Users\tamay\Downloads";
+    [string] $outputDir = "C:\Users\tamay\Desktop\home\download\exe"
 
-    $files = getFiles $targetDir
+    [string[]] $files = getFiles $targetDir
     moveFiles $files $outputDir
 }
 
-function moveFiles($files, $outputDir) {
+function moveFiles([string[]] $files, [string] $outputDir) {
     for ($i = 0; $i -lt $files.Length; $i++) {
+        # TODO すでにファイルが存在する場合の例外処理を書く
         Move-Item $files[$i] $outputDir
     }
 }
 
-function getFiles($targetDir) {
+function getFiles([string] $targetDir) {
     $files = Get-ChildItem $targetDir
     $returnFiles = @()
     for ($i = 0; $i -lt $files.Length; $i++) {
