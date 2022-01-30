fb29e5edc60b88cafea3d88e352aae85f4c40f80
gamari <gamari4552@gmail.com>
2022-01-30 20:31:16 +0900

    移動処理まで。

---
+++ README.md
@@ -0,0 +1,24 @@
+# 概要
+
+ダウンロードの整理アプリ。
+
+
+# 環境設定
+
+1. エイリアスに登録する。
+    1. Set-Alias MOVEDIR [start.psの絶対パス]
+        - http://www.vwnet.jp/windows/PowerShell/2020100601/PsAlias.htm
+2. history.jsonを用意する
+    1. start.ps1と同じディレクトリ内にhistory.jsonファイルを作る。
+    2. history.sample.jsonの内容をコピーする。
+
+
+# 機能
+
+- [ ] 
+
+# 学んだこと
+
+## PS
+
+- 移動コマンド
---
+++ execute.ps1
@@ -0,0 +1,21 @@
+﻿function Main($type, $memo) {
+    # 1. dir内のファイルをリストに格納する。
+    #   1. json形式のものだけ
+    # 2. 1.のリスト内をtempに移動する
+    
+    $target = $pwd.Path + "\target\target.json"
+    $outputDir = $pwd.Path + "\output"
+
+    move $target $outputDir
+}
+
+
+function makeSelectDirs($historys) {
+
+}
+
+
+# Main関数の呼び出し
+# $type = $args[0]
+# $memo = $args[1]
+Main
